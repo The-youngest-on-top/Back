e7bd111887b0f3e57ed4992cd89865816ebd48e3
eunseo <qnrqjrm03@naver.com>
2021-11-11 01:56:18 +0900

    회원정보 수정 api 추가

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -41,5 +41,32 @@ router.delete('/user', async (req,res)=>{
     res.send(`${user_id}   삭제`);
 });
 
+router.patch('/user', async (req,res)=>{
+    let modify = req.body.modify_id;
+    let user_id = req.body.user_id;
+    let password = req.body.password;
+    let nickname = req.body.nickname;
+    // await User.update({
+    //     comment: user_id,
+    // }, {
+    //     where: { id: 2 },
+    // });
+    if(modify==1)
+    {
+        await User.update({password:password},{where:{user_id:user_id}});
+        res.send(`${user_id}   패스워드만 수정`);
+    }
+    else if(modify==2)
+    {
+        await User.update({nickname:nickname},{where:{user_id:user_id}});
+        res.send(`${user_id}   닉네임만 수정`);
+    }
+    else if(modify==3)
+    {
+        await User.update({password:password, nickname:nickname},{where:{user_id:user_id}});
+        res.send(`${user_id}   둘다 수정`);
+    }
+});
+
 
 module.exports = router;
