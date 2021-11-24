b03614260d1dd12dd99361c84826992dff667822
kkmin223 <rkdals133@gmail.com>
2021-11-24 17:05:20 +0900

    프로필 이미지 저장 방식 추가

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,10 +1,75 @@
 const User  = require('../models/user');
 const path = require('path');
 
+// exports.signup_user = async (req,res) =>{
+//     let data = req.body;
+//     let profile_image = req.file.location;
+//     console.log(req);
+//     let e_result= await User.findOne({
+//         where:{
+//             "email": data.email
+//         }
+//     })
+//     if(e_result){
+//         res.send({
+//             "success": false,
+//             "message": "사용 불가 이메일"
+//         })
+//     }
+
+//     let p_result= await User.findOne({
+//         where:{
+//             "phone_number": data.phone_number
+//         },
+//     })
+//     if(p_result){
+//         res.send({
+//             "success": false,
+//             "message": "사용 불가 전화번호"
+//         })
+//     } 
+
+//     let n_result= await User.findOne({
+//         where:{
+//             "nickname": data.nickname
+//         },
+//     })
+//     if(n_result){
+//         res.send({
+//             "success": false,
+//             "message": "사용 불가 닉네임"
+//         })
+//     } 
+
+//     if(!(e_result || p_result || n_result))
+//     {
+//         await User.create({
+//             "id":data.user_id,
+//             "password":data.password,
+//             "profile_image": profile_image,
+//             "nickname": data.nickname,
+//             "name": data.name,
+//             "phone_number":data.phone_number,
+//             "birthday":data.birthday,
+//             "email": data.email
+//         })
+//         res.send({
+//             "success": true,
+//             "message": "회원가입 완료"
+//         })
+//         // res.send(`${data.user_id} 저장 성공`);
+//     }
+
+
+
+
+   
+// };
+
 exports.signup_user = async (req,res) =>{
     let data = req.body;
-    let profile_image = req.file.location;
-    console.log(req);
+    //let profile_image = req.file.location;
+    //console.log(req);
     try{
         let e_result= await User.findOne({
             where:{
@@ -47,7 +112,6 @@ exports.signup_user = async (req,res) =>{
             await User.create({
                 "id":data.user_id,
                 "password":data.password,
-                "profile_image": profile_image,
                 "nickname": data.nickname,
                 "name": data.name,
                 "phone_number":data.phone_number,
@@ -58,82 +122,17 @@ exports.signup_user = async (req,res) =>{
                 "success": true,
                 "message": "회원가입 완료"
             })
-        }
+            // res.send(`${data.user_id} 저장 성공`);
+        }   
     }catch(err){
         res.send({
             "success": false,
             "message": err
         });
     }
-    
-        // res.send(`${data.user_id} 저장 성공`);
-}   
-
-
-// exports.signup_user = async (req,res) =>{
-//     let data = req.body;
-//     //let profile_image = req.file.location;
-//     //console.log(req);
-//     try{
-//         let e_result= await User.findOne({
-//             where:{
-//                 "email": data.email
-//             }
-//         })
-//         if(e_result){
-//             res.send({
-//                 "success": false,
-//                 "message": "사용 불가 이메일"
-//             })
-//         }
-    
-//         let p_result= await User.findOne({
-//             where:{
-//                 "phone_number": data.phone_number
-//             },
-//         })
-//         if(p_result){
-//             res.send({
-//                 "success": false,
-//                 "message": "사용 불가 전화번호"
-//             })
-//         } 
-    
-//         let n_result= await User.findOne({
-//             where:{
-//                 "nickname": data.nickname
-//             },
-//         })
-//         if(n_result){
-//             res.send({
-//                 "success": false,
-//                 "message": "사용 불가 닉네임"
-//             })
-//         } 
-    
-//         if(!(e_result || p_result || n_result))
-//         {
-//             await User.create({
-//                 "id":data.user_id,
-//                 "password":data.password,
-//                 "nickname": data.nickname,
-//                 "name": data.name,
-//                 "phone_number":data.phone_number,
-//                 "birthday":data.birthday,
-//                 "email": data.email
-//             })
-//             res.send({
-//                 "success": true,
-//                 "message": "회원가입 완료"
-//             })
-//             // res.send(`${data.user_id} 저장 성공`);
-//         }   
-//     }catch(err){
-
-//     }
    
     
-// };
+};
 
 exports.get_user = async (req,res)=>{
     let user_id = req.body.user_id;
@@ -209,10 +208,31 @@ exports.login = async (req,res)=>{
     }
 };
 
+exports.set_profile_image = async(req,res)=>{
+    let user_id = req.body.user_id;
+    try{
+        let profile_image = req.file.location;
+        User.update({
+            "profile_image": profile_image
+        },{
+            where: {"id":user_id}
+        });
+        res.send({
+            "success": true,
+            "message": "프로필 이미지 저장 완료"
+        })
+    } catch(err){
+        res.send({
+            "success": false,
+            "message": err
+        })
+    }
+    
+}
 
 exports.get_profile_image = async (req,res)=>{
     let user_id = req.body.user_id;
-    let project_path = path.resolve("./");
+    
     let user_data= await User.findOne({
         attributes:['profile_image'],
         where:{
@@ -225,6 +245,7 @@ exports.get_profile_image = async (req,res)=>{
 
 exports.delete_user = async (req,res)=>{
     let user_id = req.body.user_id;
+    
     await User.destroy({
         where:{
             "user_id": user_id
--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const { profile_upload }= require('../models/multer');
 
 
-router.post('/user',profile_upload.single('profile_image'),controller.signup_user);
-//router.post('/user',controller.signup_user);
+//router.post('/user',profile_upload.single('profile_image'),controller.signup_user);
+router.post('/user',controller.signup_user);
 router.get('/user', controller.get_user);
 router.get('/user/login',controller.login);
 router.get('/user/profile_image', controller.get_profile_image);
+router.post('/user/profile_image', profile_upload.single('profile_image'),controller.set_profile_image);
 router.delete('/user', controller.delete_user);
 router.patch('/user', controller.modify_user);
 router.get('/user/check_id', controller.check_id);
