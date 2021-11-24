f76c985ab08f25fd0fc9b6b11de37257b90f8616
kkmin223 <rkdals133@gmail.com>
2021-11-24 18:42:03 +0900

    이미지 포함 회원가입

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { profile_upload }= require('../models/multer');
 
 
-//router.post('/user',profile_upload.single('profile_image'),controller.signup_user);
-router.post('/user',controller.signup_user);
+router.post('/user',profile_upload.single('profile_image'),controller.signup_user);
+//router.post('/user',controller.signup_user);
 router.get('/user', controller.get_user);
 router.get('/user/login',controller.login);
 router.get('/user/profile_image', controller.get_profile_image);
