9c1e813d58b2d1474a427a4bb5f0227cb53c529b
kkmin223 <rkdals133@gmail.com>
2021-11-28 18:15:17 +0900

    예약 & 장바구니 API 추가

--- src/routers/reservation_router.js
+++ src/routers/reservation_router.js
@@ -7,10 +7,10 @@ router.use(express.urlencoded({ extended: false }));
 
 router.post('/reservation',controller.add_reservation);
 router.get('/reservation/orderlist',controller.get_orderlist);
-router.get('/reservation/activity',controller.get_activity_reservation);
+router.get('/reservation/activity',controller.get_activity_reservations);
 
 
 router.post('/cart',controller.add_cart);
-router.get('/cart',controller.get_cart);
+router.get('/cart',controller.get_carts);
 
 module.exports = router;
