a9d2559532d30219543af66d142a16bdb3839bc1
eunseo <qnrqjrm03@naver.com>
2021-11-29 23:51:21 +0900

    baboo code

--- src/routers/review_router.js
+++ src/routers/review_router.js
@@ -7,5 +7,6 @@ router.get('/review', controller.get_review);
 router.get('/review/star', controller.get_star_avg);
 router.post('/review/modify', controller.modify_review);
 router.post('/review/delete', controller.delete_review);
+router.get('/review/images', controller.get_review_images);
 
 module.exports = router;
